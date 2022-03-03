44ec8fb26c19fcabe77ead5a8e276d4a0377cdfd
ashwinibalachandra10 <93797913+ashwinibalachandra10@users.noreply.github.com>
2022-03-02 22:41:29 -0800

    Update URL_reader.py

--- URL_Reader/URL_reader.py
+++ URL_Reader/URL_reader.py
@@ -3,6 +3,26 @@
 """
 Created on Tue Feb 15 16:22:40 2022
 @author: Ashwini Balachandra
+
+Description:
+Count the number of characters received (read), and stop displaying any text after it has shown 
+exactly 3000 characters. Space characters, tab characters, and newline characters are 
+characters, and should therefore be included in your count. All of your processing is in terms of 
+chunks. So, you are not allowed to count or print character by character. Instead, you must 
+increment your count by the size of the chunk of characters that you read. That size will always 
+be equal to your default chunk size, except for the very last chunk that you read from the file. 
+The last chunk that you read from the file will likely be less than your default chunk size. Since 
+your chunk size will not divide evenly into 3000, you will need to add some logic to ensure that 
+exactly 3000 characters (no more, and no less) are displayed. When you print your chunks, it is 
+okay for them to be separated by a newline character. Do not use magic numbers! The values 
+3000 and 512 should be established as named constants. In Python, the convention is to name a 
+constant by using an identifier in all caps: e.g., PRINT_LIMIT = 3000. Any other values that you 
+might use should be derived from these two named constants. The idea is for to allow for the 
+values 3000 and 512 to be adjusted (to 5000 and 256, for example), and still ensure that your 
+code operates correctly. 
+Very important! You are not allowed to calculate manually or programmatically the number of 
+iterations needed to reach your 3000 character print limit. In other words, instead of counting 
+iterations (or divisions), you need to count the number of characters that you have read so far. 
 """
 
 import urllib.request, urllib.parse, urllib.error
